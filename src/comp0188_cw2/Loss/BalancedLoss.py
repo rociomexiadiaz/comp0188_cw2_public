c5023c45c003573e78ddac727926ea0b5f83e1cf
Rocio Mexia <r.mexiadiaz@hotmail.com>
2024-12-10 17:03:16 +0000

    Update BalancedLoss.py

--- src/comp0188_cw2/Loss/BalancedLoss.py
+++ src/comp0188_cw2/Loss/BalancedLoss.py
@@ -56,7 +56,8 @@ class TrackerBalancedLoss:
             }
             loss += _loss
             if "kl" in pred.keys():
-                loss += pred["kl"]/pred["images"].shape[0]
+                kl_loss = pred["kl"]/pred["images"].shape[0]
+                loss += kl_loss
                 _metric_value_dict[f"kl_{self.name}"] = {
                     "label": f"step_{self.__step}",
                     "value": kl_loss.item()
